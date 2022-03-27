6506f539bcc093795b049612a3e8f0e8c89d35b6
lodashreload <icarusjapan@gmail.com>
2022-03-27 12:34:03 -0400

    new game button starts new game with API POST request

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,29 @@ export function App() {
   })
   function handleClickCell(row: number, column: number) {
     console.log(`You clicked on row ${row} and column ${column}`)
+    console.log({ row, column })
   }
+  async function handleNewGame() {
+    // Make a POST request to ask for a new game
+    const response = await fetch(
+      'https://sdg-tic-tac-toe-api.herokuapp.com/game',
+      {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+      }
+    )
+    if (response.ok) {
+      // Get the response as JSON
+      const newGameState = await response.json()
+      // Make that the new state!
+      setGame(newGameState)
+    }
+  }
+
   return (
     <div>
       <h1>
-        Tic Tac Toe - <button>New</button>
+        Tic Tac Toe - <button onClick={handleNewGame}>New</button>
       </h1>
       <ul>
         <li onClick={() => handleClickCell(0, 0)}>{game.board[0][0]}</li>
