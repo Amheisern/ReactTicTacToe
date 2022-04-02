ce03f69aed18244462964777d9c2edc9c10eb717
lodashreload <icarusjapan@gmail.com>
2022-04-01 21:59:20 -0400

    state down and events up

--- src/Cell.tsx
+++ src/Cell.tsx
@@ -7,9 +7,10 @@ type CellProps = {
   columnIndex: number
   recordMove: (row: number, column: number) => void
 }
-export function Cell(props: CellProps) {
+export function Cell({ cell, rowIndex, columnIndex, recordMove }: CellProps) {
   // This code allows for the deletion of .props repetition
-  const { cell, rowIndex, columnIndex, recordMove } = props
+  // const { cell, rowIndex, columnIndex, recordMove } = props
+  // above code is no longer needed since it has been declared in line in the Cell function above
   function handleClickCell() {
     // Send the event UPwards by calling the `recordMove` function we were given
     recordMove(rowIndex, columnIndex)
